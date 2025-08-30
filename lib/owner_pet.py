8dc53b03878646a9559e7530e7896801146e4b63
nyagetiria <nyagetirianaomi@gmail.com>
2025-08-30 22:31:03 +0300

    owner_pet.py

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        Pet.all.append(self)
+
+        # If owner is given, register this pet with them
+        if owner:
+            owner.add_pet(self)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Only Pet instances can be added")
+        if pet not in self._pets:
+            self._pets.append(pet)
+            pet.owner = self  # back-reference
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
